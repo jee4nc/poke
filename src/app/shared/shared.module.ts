da4713ba86631222772dda71705d1180c658fd2d
jean <jeanc.aldea1@gmail.com>
2020-03-29 18:27:47 -0300

    Add material angular and animations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SharedRoutingModule } from './shared-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './../material/material.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    MaterialModule,
+  ],
+  exports: [
+    NavbarComponent
   ]
 })
 export class SharedModule { }
